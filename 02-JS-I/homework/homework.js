a238c4d15db2af08bbdd13905c93a64f38f1b2c8
dan95 <germandanielgg@gmail.com>
2021-11-16 20:58:55 -0500

    Prueba commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 2 + 4;
+const nuevoNum = 2 + 6;
 
 // Crea una variable booleana:
 const nuevoBool = true;
